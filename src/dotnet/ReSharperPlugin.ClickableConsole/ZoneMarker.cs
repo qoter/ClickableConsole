d66c7ca983efdf2c2c9aa1cc4ecad378bee6a066
borzov <borzov@skbkontur.ru>
2022-04-15 01:09:37 +0500

    delete dotnet trash

--- src/dotnet/ReSharperPlugin.ClickableConsole/ZoneMarker.cs
+++
@@ -1,9 +0,0 @@
-using JetBrains.Application.BuildScript.Application.Zones;
-
-namespace ReSharperPlugin.ClickableConsole
-{
-    [ZoneMarker]
-    public class ZoneMarker : IRequire<IClickableConsoleZone>
-    {
-    }
-}
